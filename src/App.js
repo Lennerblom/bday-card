a1957c8d470de54f0080e43763c2c74bd81115be
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-05-01 22:20:40 -0700

    initial commit for bday card

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const stringArr = ['Today is your Birthday,  I’m glad you’re my wife;',
+  'Forty years ago today, you breathed your first breath of life.',
+  'I hope your day is happy, I hope your day is glad;',
+  'I don’t hope your day is angry, frustrated, or sad.',
+  'You’re pretty great in all the things you do;',
+  'Everyone who knows you is better off because of you.',
+  'You touch so many in one way or another;',
+  'Categories include: family, children, school and other.',
+  'I love you a lot, what more can I say;',
+  'You\'re my Honey Bunny, have a happy Birthday!',]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className='card-title'>To: Sara</h1>
+    {stringArr.map(string => <div className='poem'>{string}</div>)}
     </div>
   );
 }
