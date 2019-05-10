5cb58bed7b0b132eb1e3eb4117b000c489f25247
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-05-10 13:23:30 -0700

    Updates

---
+++ precache-manifest.95e902f2f744088afbaf834849b6e928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5ff6bb3f3007d8b48cbf571037936d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264e48e2c70b13add0c3",
+    "url": "/static/css/main.2d8858a4.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "264e48e2c70b13add0c3",
+    "url": "/static/js/main.a4663d59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a0b164de67803932e353d093cfb14a5",
+    "url": "/static/media/canvas.6a0b164d.jpg"
+  },
+  {
+    "revision": "daa264e367ee2de61f8f378cdef2fdf1",
+    "url": "/static/media/flower3.daa264e3.png"
+  }
+]);
