4edb6b396299a9423f232a759d03507fcd880b1c
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-07-19 12:22:28 -0700

    added linked list and button on lydia-letter

--- src/lydia.jsx
+++ src/lydia.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
+import LinkedList from './link_list';
 
-const letter = 'I\'m sorry that I hurt you, that’s not how fathers should be;I wish I could take it all back, but it’s too late obviously;I hope we can restore the relationship that we had;going out for driving practice, just a daughter and her dad;I know this past week has been pretty hard for you especially; Maybe it’s time we drive up highway 99 to eat at Philly-Yo-Belly.';
+const letter = 'I\'m sorry that I hurt you; that’s not how a father should be;I wish I could take it all back; but it’s too late obviously;I hope we can restore; the relationship that we had;going out for driving practice; just a daughter and her dad;I’m sorry it’s taken so long; for me to apologize for real;Maybe its time we take a drive; to enjoy a SOFT hot meal'
+let arr = letter.split(';');
+let current = '';
+let myList = new LinkedList();
+class Lydia extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    view: true
+  }
+}
+  componentDidMount(){
+    arr.map(line => myList.append(line));
+    current = myList.head.value;
+    this.forceUpdate();
+}
+  nextLine = () => {
+    if(myList.head.next === null){
+      current = arr.map(line => <div className='lydia-letter'>{line}</div>);
+      this.forceUpdate();
+    }
+    else {
+      myList.head = myList.head.next;
+      current = myList.head.value;
+      this.forceUpdate();
 
-let arr = letter.split(';')
-
-const Lydia = () => (
-
+    }
+  }
+  render() {
+    return(
+  
     <div className="App lydia">
       <head>
         <title>Lydia's Letter</title>
       </head>
-        <h1 className='card-title'>To: Lydia</h1>
-{arr.map(line => <div className='poem'>{line}</div>)}
+        <h1 className='lydia-letter'>To: Lydia</h1>
+        <div className='lydia-letter'>         
+            {current}
+        </div>
+        <button onClick={this.nextLine}>next</button>
     </div>
-)
+  )
+}
+}
 export default Lydia;
